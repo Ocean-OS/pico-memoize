f4bd819c6dda35b4e5cbc1d04793ce92c2798b04
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2024-08-25 15:47:46 -0700

    remove console logs

--- browser.js
+++ browser.js
@@ -6,7 +6,6 @@ function memoized(fn) {
             if (stack[0] === value) return "[Circular ~]"
             return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
         }
-
         return function(key, value) {
             if (stack.length > 0) {
                 let thisPos = stack.indexOf(this)
@@ -14,13 +13,11 @@ function memoized(fn) {
                     ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
                 if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
             } else stack.push(value)
-
             return replacer == null ? value : replacer.call(this, key, value)
         }
     }
     let cache = new Map();
     return function() {
-        console.log(cache);
         if (cache.has(JSON.stringify([...arguments], serializer()))) return cache.get(JSON.stringify([...arguments], serializer()));
         let result = fn(...arguments);
         cache.set(JSON.stringify([...arguments], serializer()), result);
